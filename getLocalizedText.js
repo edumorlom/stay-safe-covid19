decf0715643c407b36b1ccb18b4d43d32765610b
Eduardo Morales <AzPD5SUKpqVt>
2020-03-29 20:41:56 -0400

    Remove Unused code.

--- getLocalizedText.js
+++ getLocalizedText.js
@@ -1,8 +1,4 @@
 import en_US from './en_US'
-import es_ES from './es_ES'
-import pt_BR from './pt_BR'
-import zh_Hans from './zh_Hans'
-
 
 export default function getLocalizedText(deviceLanguage, key) {
     return en_US[key];
